02f881ef0b4cfa3b8cbdf1ee6993d7dfc5894722
arpan-kumar-saini <arpansaini55@gmail.com>
2024-02-17 23:18:09 +0530

    2 nd commit

--- script.js
+++ script.js
@@ -15,3 +15,26 @@ window.addEventListener('scroll',()=>{
 })
 
 
+
+
+// this is the tabs logic
+function openTab(evt, tabName) {
+    // Hide all tab content
+    var i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    // Deactivate all tabs
+    tabLinks = document.getElementsByClassName("tab");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].classList.remove("active");
+    }
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.classList.add("active");
+}
+// By default, open the first tab
+document.getElementById("tab1").style.display = "block";
+document.getElementsByClassName("tab")[0].classList.add("active");
+
